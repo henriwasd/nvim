c295a861b611acc42bcb5f6dd7abcf83257efcea
henriwasd <38558536+henriwasd@users.noreply.github.com>
2025-06-29 01:39:14 -0300

    new config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,33 +12,17 @@ else
   end
 end
 
-if vim.fn.executable("zsh") == 1 then
-  local clip = "/mnt/c/Windows/System32/clip.exe" -- Change this path if needed
-
-  if vim.fn.executable(clip) then
-    local opts = {
-      callback = function()
-        if vim.v.event.operator ~= "y" then
-          return
-        end
-        vim.fn.system(clip, vim.fn.getreg("0"))
-      end,
-    }
-
-    opts.group = vim.api.nvim_create_augroup("WSLYank", {})
-    vim.api.nvim_create_autocmd("TextYankPost", { group = opts.group, callback = opts.callback })
-  end
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
 end
-
--- Setting shell command flags
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
-
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
-
--- Setting shell qu
-vim.lsp.inlay_hint.is_enabled = false
