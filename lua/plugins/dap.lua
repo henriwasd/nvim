82ec0aa08367c96a51877a89b02182485259b14c
Henrique Ribeiro Rodrigues da Silva <henriwasd@gmail.com>
2026-03-09 23:06:40 -0300

    fix: use flutter-tools automatic dap setup

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,32 +3,4 @@ return {
   dependencies = {
     "suketa/nvim-dap-virtual-text",
   },
-  config = function()
-    local dap = require("dap")
-
-    local function get_flutter_path()
-      local data_path = vim.fn.stdpath("data")
-      if vim.fn.has("win32") == 1 then
-        return data_path .. "/flutter/bin/flutter.bat"
-      else
-        return data_path .. "/flutter/bin/flutter"
-      end
-    end
-
-    dap.adapters.dart = {
-      type = "executable",
-      command = get_flutter_path(),
-      args = { "debug_adapter" },
-    }
-
-    dap.configurations.dart = {
-      {
-        name = "Launch Flutter",
-        type = "dart",
-        request = "launch",
-        program = "${workspaceFolder}/lib/main.dart",
-        cwd = "${workspaceFolder}",
-      },
-    }
-  end,
 }
--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -8,11 +8,17 @@ return {
       "stevearc/dressing.nvim",
     },
     config = function()
+      local flutter_path = nil
+      if vim.fn.has("win32") == 1 then
+        flutter_path = vim.fn.stdpath("data") .. "/flutter/bin/flutter.bat"
+      end
+
       require("flutter-tools").setup({
         ui = {
           border = "rounded",
           notification_style = "plugin",
         },
+        flutter_path = flutter_path,
         decorations = {
           statusline = {
             app_version = true,
