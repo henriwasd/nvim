bd30d4a5c4a455f13ec2f9337d3608605a2be0f4
Henrique Ribeiro Rodrigues da Silva <henriwasd@gmail.com>
2026-03-09 22:58:40 -0300

    fix: use direct path for dartSdkPath

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,12 +3,15 @@ return {
   config = function()
     local dap = require("dap")
 
+    local flutter_sdk_path = vim.fn.stdpath("data") .. "/flutter"
+    local dart_sdk_path = flutter_sdk_path .. "/bin/cache/dart-sdk"
+
     dap.configurations.dart = {
       {
         type = "dart",
         request = "launch",
         name = "Launch Dart",
-        dartSdkPath = require("flutter-tools").get_sdk(),
+        dartSdkPath = dart_sdk_path,
         program = "${workspaceFolder}/bin/${workspaceFolderBasename}.dart",
         cwd = "${workspaceFolder}",
       },
@@ -16,7 +19,7 @@ return {
         type = "dart",
         request = "attach",
         name = "Attach to Dart",
-        dartSdkPath = require("flutter-tools").get_sdk(),
+        dartSdkPath = dart_sdk_path,
         observatoryPort = "${port}",
       },
     }
