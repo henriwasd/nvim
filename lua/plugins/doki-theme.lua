5906dff9653d726047e194a14e47dbae89f9bf39
henriwasd <henriwasd@gmail.com>
2026-05-25 20:50:28 -0300

    feat: doki theme

---
+++ lua/plugins/doki-theme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "doki-theme/doki-theme-vim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "zero_two_dark_obsidian",
+    },
+  },
+}
--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -2,11 +2,5 @@ return {
   { "ellisonleao/gruvbox.nvim", config = {
     transparent_mode = true,
   } },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
 }
+
