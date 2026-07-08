f836a4ae8dde109503326878a4abcb18d4899daa
henriwasd <henriwasd@gmail.com>
2026-07-07 22:51:45 -0300

    feat: add dynamic formatting config (biome vs prettier)

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { { "biome", "prettier" } },
+        typescript = { { "biome", "prettier" } },
+        javascriptreact = { { "biome", "prettier" } },
+        typescriptreact = { { "biome", "prettier" } },
+        json = { { "biome", "prettier" } },
+        jsonc = { { "biome", "prettier" } },
+      },
+      formatters = {
+        -- Biome só será executado se houver um arquivo 'biome.json' ou 'biome.jsonc' no projeto
+        biome = {
+          condition = function(ctx)
+            return vim.fs.find({ "biome.json", "biome.jsonc" }, { path = ctx.filename, upward = true })[1] ~= nil
+          end,
+        },
+        -- Prettier só será executado se o projeto NÃO estiver usando o Biome
+        prettier = {
+          condition = function(ctx)
+            local has_biome = vim.fs.find({ "biome.json", "biome.jsonc" }, { path = ctx.filename, upward = true })[1] ~= nil
+            return not has_biome
+          end,
+        },
+      },
+    },
+  },
+}
