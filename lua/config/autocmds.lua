9c110b70ca0abcc8d9e778b8d992139b0bdd209d
henriwasd <henriwasd@gmail.com>
2026-07-07 23:03:29 -0300

    feat: enable automatic reload for external file changes (agy integration)

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,28 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 
--- Exemplo: Auto-formatar ou ações específicas que não são cobertas pelo LazyVim padrão.
+-- --- AUTORELOAD DE ARQUIVOS ALTERADOS EXTERNAMENTE (ex: por Antigravity / agy) ---
+-- Garante que o Neovim recarregue o arquivo silenciosamente assim que o agy ou git fizerem alterações,
+-- desde que você não tenha modificações pendentes e não salvas no próprio buffer do Neovim.
+
+vim.opt.autoread = true
+
+local autoreload_group = vim.api.nvim_create_augroup("AutoreloadExternalChanges", { clear = true })
+
+-- Executa `:checktime` para atualizar o buffer ao ganhar foco, entrar no arquivo ou ficar ocioso
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  group = autoreload_group,
+  callback = function()
+    if vim.fn.getcmdwintype() == "" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- Notifica caso o arquivo seja atualizado com sucesso em segundo plano
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  group = autoreload_group,
+  callback = function()
+    vim.notify("Arquivo atualizado no editor (alterado no disco).", vim.log.levels.INFO, { title = "Antigravity / Git" })
+  end,
+})
