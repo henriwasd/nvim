cb725e1385765d328ff9b04a2ed7d369400e2df2
henriwasd <henriwasd@gmail.com>
2026-07-10 14:41:10 -0300

    feat: configure DAP for Flutter

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -2,9 +2,29 @@ local ok_flutter, flutter = pcall(require, "flutter-tools")
 if ok_flutter then
   flutter.setup({
     flutter_lookup_cmd = "mise where flutter",
-    debug = {
+    debugger = {
       enabled = true,
-      evaluate_to_string_in_debug_views = false,
+      run_via_dap = true,
+      register_configurations = function(paths)
+        local dap = require("dap")
+        dap.adapters.dart = {
+          type = "executable",
+          command = paths.flutter_bin,
+          args = { "debug-adapter" },
+        }
+        dap.configurations.dart = {
+          {
+            type = "dart",
+            request = "launch",
+            name = "Launch Flutter",
+            program = "${workspaceFolder}/lib/main.dart",
+            cwd = "${workspaceFolder}",
+          }
+        }
+        pcall(function()
+          require("dap.ext.vscode").load_launchjs()
+        end)
+      end,
     },
     dev_log = {
       enabled = false,
