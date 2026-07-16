841fd1f67e7f5fc3952b5a96fa6637161ea83e8d
henriwasd <henriwasd@gmail.com>
2026-07-16 08:12:09 -0300

    16395 - config neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -17,6 +17,6 @@ vim.g.autoformat = false
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
--- Habilita configurações específicas por projeto (.nvim.lua / .nvimrc)
 vim.opt.exrc = true
 
+vim.opt.relativenumber = false
